0906abc4d812e50e0209cb8ef7971f72cd386ea1
Noah Fontenette <noahfontenette@outlook.com>
2023-02-18 13:05:27 -0600

    Updated Home.js

--- my-app/src/pages/Home.js
+++ my-app/src/pages/Home.js
@@ -1,9 +1,17 @@
 import '../index.css'
+import logo from '../logo.svg'
 
 const Home = () => {
     return (
         <div>
-            <h1>Home</h1>
+            <div className='menu'>      
+                <img src={logo} alt='' className='logo'/>
+                <div className='menuitems'> 
+                    <a href='/' className='menuitem'>Home</a>
+                    <a href='/' className='menuitem'>Blog</a>
+                    <a href='/' className='menuitem'>Sources</a>
+                </div>
+            </div>
         </div>
     );
 }
